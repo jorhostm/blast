c6583987c8b04d9b66ca5b562a2a2aa1eff99335
Jørgen Rødal Høstmark <jorgenhostm@gmail.com>
2023-02-20 14:00:58 +0100

    Added console output

--- pre_process.py
+++ pre_process.py
@@ -24,17 +24,25 @@ clean_script.writelines(["#!/bin/bash\n","rm -f *.out *.msg *.abq *.cid *.com *.
 post_script = open("post_all.sh","w")
 post_script.writelines(["#!/bin/bash\n"])
 
+print("Preprocess all input files.")
+print("Materials:", materials.values())
+print("Amplitudes:", amplitudes.values())
+print("Searching for model input files...")
+
 for filename in glob.iglob('*.inp'):
    with open(filename,"r") as f:
       header = f.readline().strip('\n')
    if header == "*Heading":
+      print(f"Found model file: {filename}")
+      print("Parameterizing...")
       replacement(filename)
       model = filename.split('.')[0]
       for matk,matv in materials.items():
          for ampk,ampv in amplitudes.items():
             jobname = f"{model}_{matk}_{ampk}.inp"
+            print(f"Generating job {jobname}...")
             queue_script.writelines([f"sbatch ../jobaba {jobname}\n"])
             clean_script.writelines([f" {jobname}"])
-            post_script.writelines([f"abaqus python post_plate.py {jobname} {ampv.upper()}\n"])
+            post_script.writelines([f"abaqus python post_plate.py {model}_{matk}_{ampk} {ampv.upper()}\n"])
             with open(jobname,'w') as f:
                f.write(template.format(mat=matv, amp=ampv, model=model))
